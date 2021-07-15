44fbb8c9d9475ba20ac5715aed6d734ecd437681
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-15 12:53:22 +0300

    Connect the browser Redux dev tool with the project.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
 
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>
     ,document.getElementById("root")
 );
-
-const store = createStore(counterReducer);
